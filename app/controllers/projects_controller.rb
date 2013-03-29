f85f2a6b6ca381f75ffb03791f097e7c473fa361
asbubam <asbubam@gmail.com>
2013-03-29 16:14:54 +0900

    add 'delete project'

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,4 +16,22 @@ class ProjectsController < ApplicationController
 
 		redirect_to root_path
 	end
+
+	def destroy
+		permission_check
+
+		unless @project.destroy
+			flash[:notice] = "destroy project failed."
+		end
+		
+		redirect_to root_path
+	end
+
+protected
+	def permission_check
+		@project = Project.find(params[:id])
+		unless @project.user.id == @current_user.id
+			render :text => "forbidden"
+		end
+	end
 end
